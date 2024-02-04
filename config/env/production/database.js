b4956809c51ab86b021d368f4e853f9fc9c51d2b
Ziad-GPT <Ziad-GPT@Murads-MacBook-Pro.local>
2024-02-03 19:30:18 -0800

    using render db

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,12 +1,12 @@
 const { parse } = require("pg-connection-string");
 
 module.exports = ({ env }) => {
-  // const { host, port, database, user, password } = parse(env("DATABASE_URL"));
-  const host = env("DATABASE_HOST", "roundhouse.proxy.rlwy.net");
-  const port = env.int("DATABASE_PORT", 5432);
-  const database = env("DATABASE_NAME", "railway");
-  const user = env("DATABASE_USERNAME", "postgres");
-  const password = env("DATABASE_PASSWORD", "34E6CB6E1d4fgf15A6D1a15eFBgAec54");
+  const { host, port, database, user, password } = parse(env("DATABASE_URL"));
+  // const host = env("DATABASE_HOST", "roundhouse.proxy.rlwy.net");
+  // const port = env.int("DATABASE_PORT", 5432);
+  // const database = env("DATABASE_NAME", "railway");
+  // const user = env("DATABASE_USERNAME", "postgres");
+  // const password = env("DATABASE_PASSWORD", "34E6CB6E1d4fgf15A6D1a15eFBgAec54");
 
   return {
     connection: {
