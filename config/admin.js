81421ab563470e2b851d379c4a26cb230e64d936
Murad Qumizakis <67524607+murad-qumizakis@users.noreply.github.com>
2025-02-19 16:53:48 +0100

    changing images of readings and courses to be required

--- config/admin.js
+++ config/admin.js
@@ -1,5 +1,8 @@
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET'),
+    secret: env(
+      "ADMIN_JWT_SECRET",
+      "9294cd6ff112229cecd76eba8cea3d3e16ccb655eb1c35a6672b6bb54fc08882"
+    ),
   },
 });
